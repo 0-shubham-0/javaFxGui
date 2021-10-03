f20b33bc9673885f99dacd4555f54e12581858fa
Shubham Talawadekar <shubhamtalawadekar2002@gmail.com>
2021-10-03 18:49:08 +0530

    Initial commit

--- src/main/java/intro/javafxgui/HelloController.java
+++ src/main/java/intro/javafxgui/HelloController.java
@@ -2,10 +2,13 @@ package intro.javafxgui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField number1,number2,ans;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -13,7 +16,11 @@ public class HelloController {
     }
     @FXML
     protected void onAddButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("You click add");
+        double first = Double.parseDouble(number1.getText());
+        double second = Double.parseDouble(number2.getText());
+        double result = first + second;
+        ans.setText(String.valueOf(result));
     }
     @FXML
     protected void onSubButtonClick() {
